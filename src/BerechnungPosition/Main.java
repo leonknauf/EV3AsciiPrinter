3519fab5ccf4280cda1e2d98b0a817505086ebde
de20138 <de20138@DE02HER681041NB.de.flg.int>
2021-01-12 20:07:52 +0100

    Added Menü

--- src/BerechnungPosition/Main.java
+++ src/BerechnungPosition/Main.java
@@ -3,8 +3,6 @@ package BerechnungPosition;
 import java.util.ArrayList;
 import java.util.List;
 
-import lejos.hardware.Button;
-import lejos.hardware.lcd.LCD;
 import plott3r_V1_solved.Roboter;
 
 public class Main {
@@ -19,9 +17,10 @@ public class Main {
 		Roboter roboter;
 		
 		
+		
 		try {
 			roboter = new Roboter();
-			Blatt b =new Blatt(roboter,schriftgroesseBestimmen());
+			Blatt b =new Blatt(roboter);
 			b.druckeZeichen(zeichen);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -51,38 +50,38 @@ public class Main {
 		return drucktext;
 
 	}
-	private static int schriftgroesseBestimmen() {
-		int groesse = 5;
-
-		int id;
-		do {
-			LCD.clear();
-			LCD.drawString("Groesse Auswaehlen", 0, 0);
-			LCD.drawChar('<', 2, 2);
-			if(groesse>9) {
-				LCD.drawInt(groesse - 1, 3, 2);
-				LCD.drawInt(groesse, 5, 2);
-				LCD.drawChar((char) 94, 5, 3);
-				LCD.drawInt(groesse + 1, 7, 2);
-				LCD.drawChar('>', 9, 2);
-			}else {
-				LCD.drawInt(groesse - 1, 3, 2);
-			LCD.drawInt(groesse, 4, 2);
-			LCD.drawChar((char) 94, 4, 3);
-			LCD.drawInt(groesse + 1, 5, 2);
-			LCD.drawChar('>', 6, 2);
-			}
-			
-			id = Button.waitForAnyEvent();
-			if (id == Button.ID_RIGHT) {
-				groesse++;
-			}
-			if (id == Button.ID_LEFT) {
-				if(groesse>1)groesse--;
-			}
-		} while (id != Button.ID_ENTER);
-
-		return groesse;
-	}
+//	private static int schriftgroesseBestimmen() {
+//		int groesse = 5;
+//
+//		int id;
+//		do {
+//			LCD.clear();
+//			LCD.drawString("Groesse Auswaehlen", 0, 0);
+//			LCD.drawChar('<', 2, 2);
+//			if(groesse>9) {
+//				LCD.drawInt(groesse - 1, 3, 2);
+//				LCD.drawInt(groesse, 5, 2);
+//				LCD.drawChar((char) 94, 5, 3);
+//				LCD.drawInt(groesse + 1, 7, 2);
+//				LCD.drawChar('>', 9, 2);
+//			}else {
+//				LCD.drawInt(groesse - 1, 3, 2);
+//			LCD.drawInt(groesse, 4, 2);
+//			LCD.drawChar((char) 94, 4, 3);
+//			LCD.drawInt(groesse + 1, 5, 2);
+//			LCD.drawChar('>', 6, 2);
+//			}
+//			
+//			id = Button.waitForAnyEvent();
+//			if (id == Button.ID_RIGHT) {
+//				groesse++;
+//			}
+//			if (id == Button.ID_LEFT) {
+//				if(groesse>1)groesse--;
+//			}
+//		} while (id != Button.ID_ENTER);
+//
+//		return groesse;
+//	}
 
 }
